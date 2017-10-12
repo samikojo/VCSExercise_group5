49dbed16abbdf29a9928ce576f6c8630d6892dfb
Mikko Voima <mikko.voima@cs.tamk.fi>
2017-10-12 10:17:04 +0300

    Jump implemented

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -2,10 +2,25 @@ using UnityEngine;
 
 namespace TAMK.VCSExample
 {
-	public class Mover : MonoBehaviour
-	{
-		void Update()
-		{
-		}
-	}
+    public class Mover : MonoBehaviour
+    {
+        public bool onGround = true;
+
+        public const string JumpAxis = "Jump";
+
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Space) && onGround)
+            {
+                onGround = false;
+
+                transform.Translate(Vector3.up * 0.030f * 100f);
+            }
+        }
+
+        protected void OnCollisionStay(Collision collision)
+        {
+            onGround = true;
+        }
+    }
 }
