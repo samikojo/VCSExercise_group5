8f13fe14afe25d3ce3fcb72241e36802e036e6af
LAPTOP-2N8VPFHF\Jer Jer Binks <jerri.ahonen@cs.tamk.fi>
2017-10-12 10:26:56 +0300

    implemented rotate

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -2,10 +2,38 @@ using UnityEngine;
 
 namespace TAMK.VCSExample
 {
-	public class Mover : MonoBehaviour
-	{
-		void Update()
-		{
-		}
+    public class Mover : MonoBehaviour
+    {
+        public bool doRotate;
+
+
+        void Update()
+        {
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                Rotate(true);
+            }
+            else if (Input.GetKey(KeyCode.Q))
+            {
+                Rotate(false);
+            }
+        }
+
+        public void Rotate(bool right)
+        {
+            if (right)
+            {
+                transform.Rotate(-5, 0, -5);
+            }
+            else
+            {
+                transform.Rotate(5, 5, 5);
+            }
+            
+        }
+
+
+
 	}
 }
