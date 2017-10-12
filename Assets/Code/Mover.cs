ebceacfeb6b9c517889e714d3c0e03844a5ede8b
ilkkaTyni <ilkka.tyni@cs.tamk.fi>
2017-10-12 10:28:24 +0300

    Moving

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -4,8 +4,31 @@ namespace TAMK.VCSExample
 {
 	public class Mover : MonoBehaviour
 	{
-		void Update()
+
+        public const string HorizontalAxis = "Horizontal";
+        public const string VerticalAxis = "Vertical";
+
+        [SerializeField]
+        private float _speed = 4;
+
+        void Update()
 		{
-		}
+            Move();
+        }
+
+        private Vector3 GetInputVector()
+        {
+            float horizontalInput = Input.GetAxis(HorizontalAxis);
+            float verticalInput = Input.GetAxis(VerticalAxis);
+
+            return new Vector3(horizontalInput, 0.0f, verticalInput);
+        }
+
+        public void Move()
+        {
+            Vector3 inputVector = GetInputVector();
+            Vector3 movementVector = inputVector * _speed;
+            transform.Translate(movementVector * Time.deltaTime);
+        }
 	}
 }
